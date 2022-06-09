1507c4bd5d52817a566dd57457f696ca46ad2eae
Israr_dawar <77443384+israr96418@users.noreply.github.com>
2022-06-09 08:52:14 +0500

    Update env.py

--- sqlmodel/alembic/env.py
+++ sqlmodel/alembic/env.py
@@ -10,7 +10,7 @@ from sqlmodel import SQLModel
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-# config.set_main_option("sqlalchemy.url",f"mysql+mysqldb://isrardawar:dawar96418@localhost:3306/sqlmodel")
+# config.set_main_option("sqlalchemy.url",f"mysql+mysqldb://username:password@localhost:3306/sqlmodel")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
